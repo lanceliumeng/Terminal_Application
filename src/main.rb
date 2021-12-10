5dddaca0c773a73714aeb608fabd71d2acca1ebb
Lance Liu <lanceliumeng89@gmail.com>
2021-12-10 22:12:05 +1100

    update draft draw and create flow chart

---
+++ src/main.rb
@@ -0,0 +1,13 @@
+# require 'colorize'
+# require 'tty-color'
+# require 'tty-font'
+# require 'tty-prompt'
+# require 'tty-spinner'
+# require 'tty-progressbar'
+
+# font = TTY::Font.new(:standard)
+# pastel = Pastel.new
+# puts pastel.green(font.write("Travel",letter_spacing: 6))
+# puts font.write("Agent",letter_spacing: 5)
+# puts font.write("System",letter_spacing: 6)
+
