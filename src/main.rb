665bf99b97831d7a0662c5d23304fcbab536de32
Lance Liu <lanceliumeng89@gmail.com>
2021-12-11 13:18:11 +1100

    add welcomt method for app

--- src/main.rb
+++ src/main.rb
@@ -1,18 +1,13 @@
 require 'colorize'
 # require 'tty-color'
-# require 'tty-font'
 require 'tty-prompt'
-
 # require 'tty-spinner'
 # require 'tty-progressbar'
 
-# font = TTY::Font.new(:standard)
-# pastel = Pastel.new
-# puts pastel.green(font.write("Travel",letter_spacing: 6))
-# puts font.write("Agent",letter_spacing: 5)
-# puts font.write("System",letter_spacing: 6)
-
-
+#begin  app welcome function
+require './methods/welcome.rb'
+welcome_note
+#end
 
 prompt = TTY::Prompt.new
 puts "Welcome to our application".colorize(:light_green)
@@ -33,7 +28,4 @@ else
     puts "Please choose 1, 2, or 3, Thank you :)".colorize(:light_red)
 end
 
-# String.colors                       # return array of all possible colors names
-# String.modes                        # return array of all possible modes
-# String.color_samples                # displays color samples in all combinations
 
---
+++ src/methods/welcome.rb
@@ -0,0 +1,10 @@
+# => for apps welcome font setting
+require 'tty-font'
+
+def welcome_note
+    font = TTY::Font.new(:standard)
+    pastel = Pastel.new
+    puts pastel.green(font.write("Travel",letter_spacing: 6))
+    puts pastel.yellow(font.write("Agent",letter_spacing: 5))
+    puts pastel.blue(font.write("System",letter_spacing: 6))
+end
