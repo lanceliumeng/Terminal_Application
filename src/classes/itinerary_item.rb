6100d321008ea8e515fa27b406a7be43caa43d5a
Lance Liu <lanceliumeng89@gmail.com>
2021-12-12 10:26:55 +1100

    use TDD for travel_spec file,test ItineraryItem class

---
+++ src/classes/itinerary_item.rb
@@ -0,0 +1,20 @@
+class ItineraryItem
+
+    def initialize(name,details,price)
+        @name = name
+        @details = details
+        @price = price
+    end
+
+    def price 
+        @price
+    end
+
+    def name
+        @name
+    end
+
+    def details
+        @details
+    end
+end
--- src/spec/travel_spec.rb
+++ src/spec/travel_spec.rb
@@ -1,16 +1,18 @@
 require './classes/itinerary_item.rb'
-require './classes/itinerary.rb'
-require './classes/order.rb'
-require './classes/user.rb'
-require './classes/travel.rb'
+# require '../classes/itinerary.rb'
+# require '../classes/order.rb'
+# require '../classes/user.rb'
+# require '../classes/travel.rb'
 
 describe ItineraryItem do
     it "should return the itinerary name,details and price " do
         name = "golf tour"
         details = "5 Days,18 holes, Barnbougle Dunes, Lost Farm, LST golf clubs"
-        price = 600
+        price = 600.00
         itinerary_item = ItineraryItem.new(name,details,price)
+        expect(itinerary_item.name).to eq(name)
         expect(itinerary_item.price).to eq(price)
+        expect(itinerary_item.details).to eq(details)
     end
 end
 
